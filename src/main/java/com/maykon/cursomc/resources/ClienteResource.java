874f5e24fcff37374ed36de500d0001fb476c487
Maykon Ferreira <lmaykonf@gmail.com>
2020-08-13 18:56:41 -0300

    PU DELETE GET Cliente

--- src/main/java/com/maykon/cursomc/resources/ClienteResource.java
+++ src/main/java/com/maykon/cursomc/resources/ClienteResource.java
@@ -1,13 +1,22 @@
 package com.maykon.cursomc.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.maykon.cursomc.domain.Cliente;
+import com.maykon.cursomc.dto.ClienteDTO;
 import com.maykon.cursomc.services.ClienteService;
 
 @RestController
@@ -23,14 +32,49 @@ public class ClienteResource {
 		
 		Cliente obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
-		/*
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
-		return lista;
-		*/		
-		//return "REST esta funcionando";
+
 	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id) {
+		Cliente obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		// Caputura o URL
+		// URI uri =
+		// ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.noContent().build();
+
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<ClienteDTO>> Listar() {
+
+		List<Cliente> list = service.findAll();
+		List<ClienteDTO> listDTO = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+
+	}
+
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+
+		Page<Cliente> list = service.findPage(page, linesPerPage, orderBy, direction);
+
+		Page<ClienteDTO> listDTO = list.map(obj -> new ClienteDTO(obj));
+		return ResponseEntity.ok().body(listDTO);
+
+	}
+
+	
 }
--- src/main/java/com/maykon/cursomc/services/ClienteService.java
+++ src/main/java/com/maykon/cursomc/services/ClienteService.java
@@ -1,12 +1,20 @@
 package com.maykon.cursomc.services;
 
+import java.nio.channels.UnsupportedAddressTypeException;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.maykon.cursomc.domain.Cliente;
+import com.maykon.cursomc.dto.ClienteDTO;
 import com.maykon.cursomc.repositories.ClienteRepository;
+import com.maykon.cursomc.services.exceptions.DataIntegrityException;
 import com.maykon.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -25,4 +33,46 @@ public class ClienteService {
 
 	}
 
+	public Cliente inserir(Cliente obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	public Cliente update(Cliente obj) {
+		//obj.setId(null);
+		Cliente newObj = buscar(obj.getId());
+		updateData(newObj,obj);
+		return repo.save(newObj);
+	}
+	
+	public void delete(Integer id) {
+		//obj.setId(null);
+		buscar(id);
+		try {
+			repo.deleteById(id);
+		}
+		
+		catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir porque a há entidades relacionadas");
+		}
+		
+	}
+	public List<Cliente> findAll(){
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page,Integer linesPerPage,String orderBy,String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction) ,orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDto) {
+	
+		return new Cliente(objDto.getId(),objDto.getNome(),objDto.getEmail(),null,null);
+		//throw new UnsupportedAddressTypeException();
+	}	
+	private void  updateData(Cliente newObj,Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
+	
 }
