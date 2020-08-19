efe79481fda5c896b5c90ba06bbb844aebe4ab60
Maykon Ferreira <lmaykonf@gmail.com>
2020-08-19 18:17:39 -0300

    Esqueci a senha

--- src/main/java/com/maykon/cursomc/resources/AuthResource.java
+++ src/main/java/com/maykon/cursomc/resources/AuthResource.java
@@ -1,15 +1,19 @@
 package com.maykon.cursomc.resources;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.maykon.cursomc.dto.EmailDTO;
 import com.maykon.cursomc.security.JWTUtil;
 import com.maykon.cursomc.security.UserSS;
+import com.maykon.cursomc.services.AuthService;
 import com.maykon.cursomc.services.UserService;
 
 @RestController
@@ -19,8 +23,8 @@ public class AuthResource {
 	@Autowired
 	private JWTUtil jwtUtil;
 	
-	//@Autowired
-	//private AuthService service;
+	@Autowired
+	private AuthService service;
 	
 	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
 	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
@@ -30,11 +34,12 @@ public class AuthResource {
 		response.addHeader("access-control-expose-headers", "Authorization");
 		return ResponseEntity.noContent().build();
 	}
-	/*
+	
+	// 									@Valid é para validar preenchimento
 	@RequestMapping(value = "/forgot", method = RequestMethod.POST)
 	public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO objDto) {
 		service.sendNewPassword(objDto.getEmail());
 		return ResponseEntity.noContent().build();
 	}
-	*/
+	
 }
