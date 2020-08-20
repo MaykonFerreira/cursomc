d1e800d20d3c08fb173e7bda0a834c10f4c14244
Maykon Ferreira <lmaykonf@gmail.com>
2020-08-20 13:56:52 -0300

    Salvando o primeiro arquivo no S3

--- src/main/java/com/maykon/cursomc/services/EmailService.java
+++ src/main/java/com/maykon/cursomc/services/EmailService.java
@@ -4,6 +4,7 @@ import javax.mail.internet.MimeMessage;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.maykon.cursomc.domain.Cliente;
 import com.maykon.cursomc.domain.Pedido;
 
 public interface EmailService {
@@ -17,5 +18,7 @@ public interface EmailService {
 	void sendOrderConfirmationHtmlEmail(Pedido obj);
 	
 	void envioHtmlEmail(MimeMessage msg);
+	
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 	
 }
